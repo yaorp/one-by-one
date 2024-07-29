3ebc75838963b6652b3fa34e57975ddff2a623da
yaorp <yao_rp@qq.com>
2024-07-29 09:55:02 +0800

    Initial commit

---
+++ src/main/java/com/springcloud/ms/OneByOneApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yaorp
+ */
+@SpringBootApplication
+public class OneByOneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneByOneApplication.class, args);
+    }
+}
