943a65d8b0898aa4152268b925079bca8872848a
yaorp <yao_rp@qq.com>
2024-08-21 17:48:00 +0800

    新增插入排序

--- src/main/java/com/springcloud/ms/controller/algorithm/InsertSort.java
+++ src/main/java/com/springcloud/ms/controller/algorithm/InsertSort.java
@@ -22,7 +22,8 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int a[] = { 38,65,97,76,13,27,49 };
+
         insertSort(a);
-        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a) );
     }
 }
