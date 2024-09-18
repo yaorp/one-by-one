8fc1d582b0acaeef5d0921e1b4c62575d4f90b22
yaorp <yao_rp@qq.com>
2024-09-18 17:23:22 +0800

    新增

--- src/main/java/com/springcloud/ms/controller/algorithm/ClimbStairs.java
+++ src/main/java/com/springcloud/ms/controller/algorithm/ClimbStairs.java
@@ -8,7 +8,11 @@ package com.springcloud.ms.controller.algorithm;
 public class ClimbStairs {
 
 
-
+    /**
+     * 滚动数组思想 后面的一位的值是前面两位的和，然后把值往前移动
+     * @param n
+     * @return
+     */
     public int climbStairs(int n){
         int q=0,p=0,s=1;
         for (int i=1;i<=n;i++){
@@ -18,4 +22,24 @@ public class ClimbStairs {
         }
         return s;
     }
+
+    /**
+     * 最后一步要么是怕一节，要么是爬两节，也就是一阶过来方法数+两节过来方法数
+     * f(n)=f(n-1)+f(n-2)
+     */
+    public int climbStairs2(int n){
+        int maxType = 0;
+        if (n==1){
+            return 1;
+        }
+
+        if (n==2){
+            return 2;
+        }
+
+//        for (int i=3;i<;i)
+        maxType = climbStairs(n-1)+ climbStairs(n-2);
+        return maxType;
+    }
+
 }
